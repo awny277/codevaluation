6c1092ef6bb8f751e9146b1b2aee395720d25e89
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-10-18 23:56:27 +0200

    update

--- pages/dashboard-src.js
+++ pages/dashboard-src.js
@@ -1,6 +1,20 @@
 import React from "react";
+import useSWR from "swr";
+const fetcher = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = response.json();
+  return data;
+};
 const DashBoardSrc = () => {
-  return <>Hello from src</>;
+  const { data, error } = useSWR("dashboard", fetcher);
+  if (error) return "An Error";
+  if (!data) return "Loading ....";
+
+  return (
+    <>
+      <h2>DashBoard</h2>
+    </>
+  );
 };
 
 export default DashBoardSrc;
